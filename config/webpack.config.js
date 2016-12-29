fca946597f6c20890267b6281050278db646cb4c
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-29 21:50:03 +1100

    Use babel to compile the client side code
    By hooking babel into webpack we can now write ES6 on the client as we
    could on the server.
    
    Also adding babel-core as babel-loader requires it as a peerDependency
    even though this project doesn't use it.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     devtool: "#inline-source-map",
     "entry": "./src/client",
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    },
     "output": {
         filename: './public/bundle.js'
     }
