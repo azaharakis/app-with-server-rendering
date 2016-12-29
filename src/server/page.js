470f8dc04888108bec51c573a5e7f93dc19aec00
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-29 16:14:44 +1100

    Express the application's response with a valid Document
    - renamed server to index within the server directory to signify the intent as that file being the default export
    - page.js becomes a [templated string](https://github.com/azaharakis/ui-training/tree/master/src/1.%20Introduction%20to%20ES%206/5.%20Template%20Strings) with the response
     - we've moved the callback function that sends the response to a module called middleware, in an effort to keep the server logic about setting up express

---
+++ src/server/page.js
@@ -0,0 +1,11 @@
+export default `
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
+    </head>
+    <body>
+        Hello world
+    </body>
+</html>
+`;
