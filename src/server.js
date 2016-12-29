cdafb1dc3a17af2ce73f37ff61fe5ce8498c10e4
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-29 15:31:55 +1100

    Add an express server
    This servers a basic response, that responds with hello world
    listening on the port 3001 unless specified via an environment variable
    
    Ignoring the lint rule console log as it is helpful in this instance
    that we output a message so we can see that the server has started

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
-(() => {
-    console.log('hello world');
-})();
+/* eslint-disable no-console */
+
+import express from 'express';
+
+const app = express();
+
+app.set('host', process.env.HOST || "0.0.0.0");
+app.set('port', process.env.PORT || 3001);
+
+app.get('/', (request, response) => {
+    response.status(200).send('hello world');
+});
+
+app.listen(app.get('port'), app.get('host'), () => {
+    console.log(`server listening on http://${app.get('host')}:${app.get('port')}`);
+});
