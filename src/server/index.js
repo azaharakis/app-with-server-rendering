d80d78774f2b7a999a2cf3772ca48c9b7be1eaad
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-29 16:14:44 +1100

    Express the application's response with a valid Document
    - renamed server to index within the server directory to signify the intent as that file being the default export
    - page.js becomes a [templated string](https://github.com/azaharakis/ui-training/tree/master/src/1.%20Introduction%20to%20ES%206/5.%20Template%20Strings) with the response
     - we've moved the callback function that sends the response to a module called middleware, in an effort to keep the server logic about setting up express

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-console */
 
 import express from 'express';
+import middleware from './middleware';
 
 const app = express();
 
 app.set('host', process.env.HOST || "0.0.0.0");
 app.set('port', process.env.PORT || 3001);
 
-app.get('/', (request, response) => {
-    response.status(200).send('hello world');
-});
+app.get('/', middleware);
 
 app.listen(app.get('port'), app.get('host'), () => {
     console.log(`server listening on http://${app.get('host')}:${app.get('port')}`);
