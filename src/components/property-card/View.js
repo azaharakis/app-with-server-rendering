7dc2edab176c978346101b08ccda771d9946fc8c
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-30 20:38:59 +1100

    Style the application and components
    You can see the css is sitting along side the components that use it.
    Hopefully you can see how the code is starting to be fairly portable
     and encapsulated within their corresponding directories.
    
    The css is using a naming convention called BEM
    https://github.com/azaharakis/ui-training/blob/master/src/4.%20Writing%20css%20at%20scale/README.md

--- src/components/property-card/View.js
+++ src/components/property-card/View.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './main.css';
 
 export default ({image, address, type}) => (
-    <div>
-        <img src={image} />
-        <span>{type}</span>
-        <p>{address}</p>
+    <div className="property-card">
+        <img className="property-card__image" src={image} />
+        <div className="property-card__content">
+            <span>{type}</span>
+            <p className="property-card__address">{address}</p>
+        </div>
     </div>
 )
