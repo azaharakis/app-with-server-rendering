16576799c1396cd186d5220faac24827a09729fb
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-30 14:15:04 +1100

    Create an App component
    This component replaces the hardcoded 'Hello World' Text in the
    templated string and we are now at the point where we have the client
    and server entry point's in sync rendering the same react component
    tree.
    
    This means that the server is now rendering the initial markup and
    handing off to the client to bind DOM events, and take over any DOM
    manipulation on the client if we add them

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,5 +1,12 @@
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
+import App from '../components/App';
+
 import page from './page';
 
 export default (request, response) => {
-    return response.status(200).send(page);
+    const applicationString = ReactDOMServer.renderToString(<App />);
+    const pageResponse = page(applicationString);
+
+    return response.status(200).send(pageResponse);
 }
--- src/server/page.js
+++ src/server/page.js
@@ -1,11 +1,11 @@
-export default `
+export default (app) => `
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
     </head>
     <body>
-        Hello world
+        <div id="wrapper">${app}</div>
         <script src="/public/bundle.js"></script>
     </body>
 </html>
