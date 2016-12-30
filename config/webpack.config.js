66330a0a74643a7c3a2544f4c2bf46230a48edc6
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-30 20:00:49 +1100

    Add some hardcoded data and render some properties
    Because the hardcoded data is a json file and we have setup our
    application to render on both the server and the client, we need to
    setup webpack to deal with json files.
    
     Ideally we'll be getting out data from an API. But for the sake of
     demonstration We'll setup our presentation first before we work on
     setting up API requests
    
     App introduces some additional concepts
     ### using array map
     Given the mock data we are able to map over the array and return a new
     one which is each object in the array represented as a react
     "property-card"
     https://github.com/azaharakis/ui-training/tree/master/src/1.%20Introduction%20to%20ES%206/7.%20Array%20Operations#map
    
     ### JSX spread notation
     https://gist.github.com/sebmarkbage/07bbe37bc42b6d4aef81
     A convinient way to pass in props to a react component is to use a spread.
     Every top level property on that object is available as a prop to the component.
    
     ### Key https://facebook.github.io/react/docs/lists-and-keys.html
     This helps react understand how to apply any changes if the list were
     to change.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,10 +2,16 @@ module.exports = {
     devtool: "#inline-source-map",
     "entry": "./src/client",
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader'
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
     },
     "output": {
         filename: './public/bundle.js'
