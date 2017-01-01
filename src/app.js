5adc8d955ee19bbec9816f4ef2a4a483283ba00f
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-01-01 19:37:58 +1100

    Setting up an API call using Fetch
    While the Fetch API is still an experimental technology, its
    generally used in the industry as a light weight way to make API
    call's it's also already supported by some browsers. For those
    browsers that don't support it we are using a polyfill. Also as we
    will eventually make API calls on the server it's a good idea to have
    an API that is consistent across both client and server. Which is
    another reason for this choice of polyfill.
    
    We have chosen to implement the fetching of data from the API in a
    lifecycle method of the App component. `componentDidMount` executes
    immediately after the initial render on the client.
    
    https://github.com/azaharakis/ui-training/tree/master/src/3.%20React/2.%20Managing%20State
    Explains how the App component uses state to update the view of the
    client once the response has returned.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('./shared/polyfills');
 require('./server/ignoreExtensions');
 require('babel-register');
 require('./server');
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
 // @flow
 
 import './main.css';
-import React from 'react';
+import React, { Component } from 'react';
 import PropertyCard from './property-card';
-import mockData from '../server/mockData.json'
 import type { Property } from '../types/Property';
 
-export default () => {
-    return (
-        <div>
-            {mockData.map((property: Property, key) => <PropertyCard key={key} {...property} />)}
-        </div>
-    );
+export default class App extends Component {
+    state: {
+        loading: boolean,
+        properties: Array<Property>
+    }
+
+    constructor () {
+        super();
+        this.state = {
+            loading: true,
+            properties: []
+        }
+    }
+
+    componentDidMount () {
+        fetch('http://localhost:3001/api')
+            .then(response => response.json())
+            .then(properties => {
+                this.setState({
+                    loading: false,
+                    properties
+                })
+            });
+    }
+
+    renderProperties () {
+        return this.state.properties.map((property: Property, key) => <PropertyCard key={key} {...property} />)
+    }
+
+    render () {
+        return (
+            <div>
+                { this.state.loading ? 'Loading...' : this.renderProperties() }
+            </div>
+        );
+    }
 }
