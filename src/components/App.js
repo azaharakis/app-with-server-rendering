7707e5c83da048430ded64d56922565262b901f1
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-01-01 17:51:51 +1100

    Setup Flow
    A basic implementation to show how flow could be added incrementally
    to a project. We've chosen to create a simple Property type to see
    how it can help when seeing the shape of the object required when
    passing props into components
    
     Each file you wish to type check should have the flow directive
     added to the top of the file. If you are using index.js files to
     export modules it's also important to add flow to these files,
     otherwise type checks wont occur when consuming the types.
    
     Regarding the flow config for simple setups often nothing is
     required to be added, find out more here:
     https://flowtype.org/docs/advanced-configuration.html
    
     We also setup a task to run flow and add an additional test task so
     we can group our our quality control checks.
    
     A couple of dependencies have also been introduced, babel-eslint is
     a parser needed as ESlint's parser doesn't support type annotations
     The eslint-plugin-flow type is used to apply linting rules to our
     type annotations. It's configured to use the recommended settings:
     https://github.com/gajus/eslint-plugin-flowtype

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
+// @flow
+
 import './main.css';
 import React from 'react';
 import PropertyCard from './property-card';
 import mockData from '../server/mockData.json'
+import type { Property } from '../types/Property';
 
 export default () => {
     return (
         <div>
-            {mockData.map((property, key) => <PropertyCard key={key} {...property} />)}
+            {mockData.map((property: Property, key) => <PropertyCard key={key} {...property} />)}
         </div>
     );
 }
