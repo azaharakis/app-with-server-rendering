bc3564a5ee9c6d9b22b14e1f1af4d36aea3c5308
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-01-02 17:04:16 +1100

    Fetch API data on the server
    One of the benefits of having a server is that we can set it up to
    render a full page response. The problem with our last commit
    was that we required the user to download a javascript package
    to make a request to an API to display a list of properties.
    Meaning
    - We unnecessarily delay the rendering of meaningful content
    - We make it harder for SEO Crawlers to access our content.
    
    With a few adjustments we can change our implementation so that we
    make the API call on the server. Populate the data within a variable as
    part of the payload we send to the browser, and then use that value to
    "hydrate" our application for the client.
    
    Because the API request is async we need to execute it at part of the
    application lifecycle that will wait before sending a response. Thus we
    create a static method on the React class, which we can execute without
    an instance from the middleware module. Once the data has returned we
    then resolve the Promise and send a request to the browser.

--- src/components/App.js
+++ src/components/App.js
@@ -4,40 +4,27 @@ import './main.css';
 import React, { Component } from 'react';
 import PropertyCard from './property-card';
 import type { Property } from '../types/Property';
+import type { Fetch } from 'isomorphic-fetch';
 
 export default class App extends Component {
-    state: {
-        loading: boolean,
-        properties: Array<Property>
+    props: {
+        data: Array<Property>
     }
 
-    constructor () {
-        super();
-        this.state = {
-            loading: true,
-            properties: []
-        }
-    }
-
-    componentDidMount () {
-        fetch('http://localhost:3001/api')
+    static fetchData (): Fetch {
+        return fetch('http://localhost:3001/api')
             .then(response => response.json())
-            .then(properties => {
-                this.setState({
-                    loading: false,
-                    properties
-                })
-            });
+            .then(properties => properties);
     }
 
     renderProperties () {
-        return this.state.properties.map((property: Property, key) => <PropertyCard key={key} {...property} />)
+        return this.props.data.map((property: Property, key) => <PropertyCard key={key} {...property} />)
     }
 
     render () {
         return (
             <div>
-                { this.state.loading ? 'Loading...' : this.renderProperties() }
+                { this.renderProperties() }
             </div>
         );
     }
